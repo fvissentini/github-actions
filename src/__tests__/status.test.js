9a66a76f239296d80d220c5968432bf42883d4c7
Fernando Vissentini <fernando.vissentini@element.in>
2020-11-19 16:49:30 +0100

    failing test

--- src/__tests__/status.test.js
+++ src/__tests__/status.test.js
@@ -7,7 +7,7 @@ describe("status test", () => {
   it("when status is called in then return ok", async () => {
     const response = await get({});
     expect(response.statusCode).toEqual(200)
-    expect(response.body).toEqual('It is ALIVE');
+    expect(response.body).toEqual('It is ALIVE1');
   });
 
 });
